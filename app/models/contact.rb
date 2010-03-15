49f42f4bea1a0379ab55829f787c42b940b23312
Adam Hościło <adamh@ahMBP.local>
2010-03-15 17:06:16 +0100

    Create contacts

---
+++ app/models/contact.rb
@@ -0,0 +1,30 @@
+class Contact < ActiveRecord::Base
+
+  hobo_model # Don't put anything above this
+
+  fields do
+    title :string, :required
+    email :email_address, :required
+    timestamps
+  end
+
+
+  # --- Permissions --- #
+
+  def create_permitted?
+    acting_user.administrator?
+  end
+
+  def update_permitted?
+    acting_user.administrator?
+  end
+
+  def destroy_permitted?
+    acting_user.administrator?
+  end
+
+  def view_permitted?(field)
+    true
+  end
+
+end
