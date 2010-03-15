4421d1739292b7cd4a0a40916d78563bc18b1ee2
Adam Hościło <adamh@ahMBP.local>
2010-03-15 23:18:43 +0100

    inactive consultations index action added
    index-page for consultations

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -3,5 +3,15 @@ class ConsultationsController < ApplicationController
   hobo_model_controller
 
   auto_actions :all
+  
+  index_action :inactive
+  
+  def index
+    hobo_index(model.active)
+  end
+  
+  def inactive
+    hobo_index(model.inactive)
+  end
 
 end
